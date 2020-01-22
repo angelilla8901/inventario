3e773a942bae9e39dfc962616e52b3276485879c
angela <angelargarciar@gmail.com>
2020-01-21 21:14:14 -0500

    menu, routing, orden form

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {OrdenComponent} from './orden/orden.component';
+
+
+const routes: Routes = [
+  { path: 'orden', component: OrdenComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
