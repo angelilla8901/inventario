9f0f490a35ffe1e6976350b48b136449fa719125
angela <angelargarciar@gmail.com>
2020-02-07 20:14:05 -0500

    added flex behavior

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OrdenComponent } from './orden/orden.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { OrdenComponent } from './orden/orden.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/orden/orden.component.ts
+++ src/app/orden/orden.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+
+const ELEMENT_DATA: OrdenCompra[] = [
+  {codigo: 'AS1254', nombre: 'Tornillo', cantidad: 5, unidad: 'Cajax100', entregados: 3, pendientes: 2},
+  {codigo: 'ER5455', nombre: 'Cable', cantidad: 100, unidad: 'mts', entregados: 100, pendientes: 0}
+];
 
 @Component({
   selector: 'app-orden',
@@ -7,9 +13,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdenComponent implements OnInit {
 
-  constructor() { }
+  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Código', 'Nombre', 'Cantidad', 'Unidad', 'Entregados', 'Pendientes'];
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+}
+
+export interface OrdenCompra {
+  codigo: string;
+  nombre: string;
+  cantidad: number;
+  unidad: string;
+  entregados: number;
+  pendientes: number;
 }
